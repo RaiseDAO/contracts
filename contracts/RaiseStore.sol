46cf44c4fa592a990f5b407d634951b20a346fbd
asimaranov <a.simaranov@gmail.com>
2022-11-14 13:39:36 +0300

    Migrated raise pay, raise store, raise hide, raise paywall to the single contract

--- contracts/RaiseStore.sol
+++ contracts/RaiseStore.sol
@@ -25,34 +25,103 @@ contract RaiseStore is ERC1155, Ownable {
         uint32 amount;
         address payToken;
         uint256 itemPrice;
+        bytes32 additionalInfo;
+    }
+
+    struct ShopItem {
+        uint128 shopUUID;
+        uint128 itemUUID;
+        address sellerAddr;
+        address payToken;
+        bool isAvaiable;
+        uint256 itemPrice;
+        bytes32 additionalInfo;
     }
 
     event UserBought(address sellerAddr, UserOrder order);
+    event TokenWhitelisted(address token);
+    event TokenBlacklisted(address token);
+    event ItemRegistered(uint128 itemUUID, ShopItem item);
+    event ItemEdited(uint128 itemUUID, ShopItem item);
+    event ItemDeleted(uint128 itemUUID);
 
     uint256 public serviceFeePromille;
     mapping(address => bool) public whitelistedTokens;
+    mapping(uint128 => ShopItem) public registeredItems;
 
     constructor(uint256 serviceFeePromille_) ERC1155("https://api.raisepay.io/item/{id}.json") {
         serviceFeePromille = serviceFeePromille_;
     }
 
+    function changeNftUrl(string calldata uri_) public onlyOwner {
+        _setURI(uri_);
+    }
+
     function whitelistTokens(address[] calldata tokens) public onlyOwner {
-        for(uint i = 0; i < tokens.length; i++) {
+        for(uint256 i = 0; i < tokens.length; i++) {
             whitelistedTokens[tokens[i]] = true;
+            emit TokenWhitelisted(tokens[i]);
         }
     }
 
     function blacklistTokens(address[] calldata tokens) public onlyOwner {
-        for(uint i = 0; i < tokens.length; i++) {
+        for(uint256 i = 0; i < tokens.length; i++) {
             whitelistedTokens[tokens[i]] = false;
+            emit TokenBlacklisted(tokens[i]);
         }
     }
 
-    function buy(UserOrder calldata order) public {
+    function registerItems(ShopItem[] calldata items) public {
+        for(uint256 i = 0; i < items.length; i++) {
+            ShopItem memory item = items[i];
+
+            require(item.itemUUID != 0, "Item id can't be zero");
+            require(registeredItems[item.itemUUID].itemUUID == 0, "Item already registered");
+            require(whitelistedTokens[item.payToken], "Token is not whitelisted");
+
+            registeredItems[item.itemUUID] = item;
+            emit ItemRegistered(item.itemUUID, item);
+        }
+    }
+
+    function editRegisteredItems(ShopItem[] calldata items) public {
+        for(uint256 i = 0; i < items.length; i++) {
+            ShopItem memory item = items[i];
+
+            require(item.itemUUID != 0, "Item id can't be zero");
+            require(msg.sender == registeredItems[item.itemUUID].sellerAddr, "Only owner can edit item");
+            require(registeredItems[item.itemUUID].itemUUID != 0, "Item not found");
+            require(whitelistedTokens[item.payToken], "Token is not whitelisted");
+
+            registeredItems[item.itemUUID] = item;
+            emit ItemEdited(item.itemUUID, item);
+        }
+    }
+
+    function deleteRegisteredItems(uint128[] calldata itemsToDelete) public {
+        for(uint256 i = 0; i < itemsToDelete.length; i++) {
+            require(msg.sender == registeredItems[itemsToDelete[i]].sellerAddr, "Only owner can edit item");
+
+            delete registeredItems[itemsToDelete[i]];
+
+            emit ItemDeleted(itemsToDelete[i]);
+        }
+    }
+
+    function buy(UserOrder calldata order, bool allowUnregisteredItems) public {
         for(uint256 i = 0; i < order.items.length; i++) {
             OrderItem memory item = order.items[i];
 
             require(whitelistedTokens[item.payToken], "Token is not whitelisted");
+
+            if (!allowUnregisteredItems) {
+                ShopItem memory registeredItem = registeredItems[item.itemUUID];
+
+                require(registeredItem.payToken == item.payToken, "Invalid paytoken");
+                require(registeredItem.itemPrice == item.itemPrice, "Invalid price");
+                require(registeredItem.sellerAddr == order.sellerAddr, "Invalid seller address");
+                require(registeredItem.isAvaiable, "Item is not available");
+            }
 
             _mint(msg.sender, item.itemUUID, item.amount, "");
             uint256 sum = item.amount * item.itemPrice;
@@ -68,72 +137,29 @@ contract RaiseStore is ERC1155, Ownable {
     function withdraw(address tokenAddr, uint256 amount) public onlyOwner {
         IERC20(tokenAddr).safeTransfer(msg.sender, amount);
     }
-}
-
-contract RevealableErc1155 is ERC1155, Ownable {
-    using SafeERC20 for IERC20;
-
-    event NftCreated(address user, uint256 nftId);
-
-    uint256 public serviceFeePromille;
-    mapping(address => bool) public whitelistedTokens;
-    NftInfo[] public nfts;
-    
-    struct NftInfo {
-        bytes16 imageHash;
-        address token;
-        uint256 price;
-        address sellerAddr;
-    }
-    constructor(uint256 serviceFeePromille_) ERC1155("https://api.raisepay.io/nft/{id}.json") {
-        serviceFeePromille = serviceFeePromille_;
-    }
-
-    function changeNftUrl(string calldata uri_) public onlyOwner {
-        _setURI(uri_);
-    }
-
-    function createNft(bytes16 imageHash, address tokenAddr, uint256 price) public returns (uint256 nftId) {
-        nftId = nfts.length;
-        emit NftCreated(msg.sender, nftId);
-        nfts.push(NftInfo(imageHash, tokenAddr, price, msg.sender)); 
-    }
-
-    function buyNft(uint256 id) public {
-        require(id < nfts.length, "No nft with such id");
-        NftInfo memory nft = nfts[id];
-        
-        uint256 fee = nft.price * serviceFeePromille / 1000;
-
-        IERC20(nft.token).safeTransferFrom(msg.sender, nft.sellerAddr, nft.price - fee);
-        IERC20(nft.token).safeTransferFrom(msg.sender, address(this), fee);
-
-        _mint(msg.sender, id, 1, "");
-    }
-
-    function withdraw(address tokenAddr, uint256 amount) public onlyOwner {
-        IERC20(tokenAddr).safeTransfer(msg.sender, amount);
-    }
 
     function checkSignature(
         address userAddress, 
-        uint256 nftId, 
+        uint128 itemUUID, 
         uint8 v, 
         bytes32 r, 
         bytes32 s, 
         uint256 timestamp, 
         uint256 maxTimeDelta
     ) 
-        public view returns (bool) 
+        public view returns (bool)
     {
-        require(nftId < nfts.length, "No nft with such id");
+        
+        ShopItem memory registeredItem = registeredItems[itemUUID];
 
-        uint256 balance = balanceOf(userAddress, nftId);
+        require(registeredItem.itemUUID != 0, "Item not found");
+
+        uint256 balance = balanceOf(userAddress, itemUUID);
         
         require(balance > 0, "User haven't got nft");
 
         bytes memory prefix = "\x19Ethereum Signed Message:\n49";
-        bytes32 prefixedProof = keccak256(abi.encodePacked(prefix, "RevealableErc1155", timestamp));
+        bytes32 prefixedProof = keccak256(abi.encodePacked(prefix, "RaiseStore", timestamp));
 
         address signer = ecrecover(prefixedProof, v, r, s);
         require(signer == userAddress, "Invalid signer");
